700efd4ebcb20ec44984bfc4d2bf6a54f3dcb771
katagaki <68231035+katagaki@users.noreply.github.com>
2024-05-20 23:40:31 +0900

    Improve performance of chart loading

--- DJDX/Views/Analytics/ChartsView.swift
+++ DJDX/Views/Analytics/ChartsView.swift
@@ -18,34 +18,30 @@ struct ChartsView: View {
 
     @AppStorage(wrappedValue: 1, "SelectedLevelFilterForScoreRateInAnalyticsView") var levelFilterForScoreRate: Int
 
-    let orderOfScoreRates: [String] = ["F", "E", "D", "C", "B", "A", "AA", "AAA"]
+    @State var isInitialScoresLoaded: Bool = false
+    @State var clearLampPerDifficulty: [Int: [String: Int]] = [:] // [Difficulty: [Clear Type: Count]]
+    @State var scoresPerDifficulty: [Int: [String: Int]] = [:] // [Difficulty: [DJ Level: Count]]
+    let djLevels: [String] = ["F", "E", "D", "C", "B", "A", "AA", "AAA"]
 
     var body: some View {
         NavigationStack(path: $navigationManager.analyticsTabPath) {
             List {
                 Section {
-                    Chart(songRecords) { songRecord in
-                        let scores: [ScoreForLevel] = [songRecord.beginnerScore,
-                                                       songRecord.normalScore,
-                                                       songRecord.hyperScore,
-                                                       songRecord.anotherScore,
-                                                       songRecord.leggendariaScore]
-                        ForEach(scores, id: \.level.rawValue) { score in
-                            if score.difficulty != 0 {
-                                BarMark(
-                                    x: .value("LEVEL", score.difficulty),
-                                    y: .value("CLEAR COUNT", score.clearType != "NO PLAY" ? 1 : 0),
-                                    width: .fixed(10.0)
-                                )
-                                .foregroundStyle(
-                                    by: .value("CLEAR TYPE", score.clearType)
-                                )
-                                .position(
-                                    by: .value("CLEAR TYPE", score.clearType),
-                                    axis: .horizontal,
-                                    span: .ratio(1)
-                                )
-                            }
+                    Chart(Array(clearLampPerDifficulty.keys), id: \.self) { difficulty in
+                        ForEach(clearLampPerDifficulty[difficulty]!.sorted(by: <), id: \.key) { clearType, count in
+                            BarMark(
+                                x: .value("LEVEL", difficulty),
+                                y: .value("CLEAR COUNT", count),
+                                width: .fixed(10.0)
+                            )
+                            .foregroundStyle(
+                                by: .value("CLEAR TYPE", clearType)
+                            )
+                            .position(
+                                by: .value("CLEAR TYPE", clearType),
+                                axis: .horizontal,
+                                span: .ratio(1)
+                            )
                         }
                     }
                     .chartLegend(.visible)
@@ -55,13 +51,12 @@ struct ChartsView: View {
                     .chartXScale(domain: 1...12)
                     .chartForegroundStyleScale([
                         "FULLCOMBO CLEAR": .white,
-                        "FAILED": .red,
+                        "CLEAR": .cyan,
                         "ASSIST CLEAR": .purple,
                         "EASY CLEAR": .green,
-                        "CLEAR": .cyan,
                         "HARD CLEAR": .pink,
                         "EX HARD CLEAR": .yellow,
-                        "NO PLAY": .gray
+                        "FAILED": .red
                     ])
                     .frame(height: 200.0)
                     .listRowInsets(.init(top: 18.0, leading: 18.0, bottom: 18.0, trailing: 18.0))
@@ -70,7 +65,7 @@ struct ChartsView: View {
                         .font(.body)
                 }
                 Section {
-                    Picker(selection: $levelFilterForScoreRate) {
+                    Picker(selection: $levelFilterForScoreRate.animation()) {
                         Text("LEVEL 1").tag(1)
                         Text("LEVEL 2").tag(2)
                         Text("LEVEL 3").tag(3)
@@ -86,26 +81,16 @@ struct ChartsView: View {
                     } label: {
                         Text("レベル")
                     }
-                    Chart(songRecords) { songRecord in
-                        let scores: [ScoreForLevel] = [songRecord.beginnerScore,
-                                                       songRecord.normalScore,
-                                                       songRecord.hyperScore,
-                                                       songRecord.anotherScore,
-                                                       songRecord.leggendariaScore]
-                        ForEach(scores, id: \.level.rawValue) { score in
-                            if score.difficulty != 0 &&
-                                score.difficulty == levelFilterForScoreRate &&
-                                score.djLevel != "---" {
-                                BarMark(
-                                    x: .value("LEVEL", score.djLevel),
-                                    y: .value("CLEAR COUNT", score.clearType != "NO PLAY" ? 1 : 0),
-                                    width: .fixed(10.0)
-                                )
-                            }
-                        }
+                    Chart(scoresPerDifficulty[levelFilterForScoreRate]?.sorted(by: <) ??
+                          [:].sorted(by: <), id: \.key) { djLevel, count in
+                        BarMark(
+                            x: .value("DJ LEVEL", djLevel),
+                            y: .value("CLEAR COUNT", count),
+                            width: .fixed(10.0)
+                        )
                     }
                     .chartLegend(.visible)
-                    .chartXScale(domain: orderOfScoreRates)
+                    .chartXScale(domain: djLevels)
                     .frame(height: 200.0)
                     .listRowInsets(.init(top: 18.0, leading: 18.0, bottom: 18.0, trailing: 18.0))
                 } header: {
@@ -114,6 +99,73 @@ struct ChartsView: View {
                 }
             }
             .navigationTitle("プレー分析")
+            .refreshable {
+                withAnimation {
+                    reloadScores()
+                }
+            }
+            .task {
+                if !isInitialScoresLoaded {
+                    isInitialScoresLoaded = true
+                    withAnimation {
+                        reloadScores()
+                    }
+                }
+            }
+        }
+    }
+
+    func reloadScores() {
+        withAnimation {
+            clearLampPerDifficulty.removeAll()
+            scoresPerDifficulty.removeAll()
+        }
+        Task.detached {
+            let songRecords = await songRecords
+
+            var newClearLampPerDifficulty: [Int: [String: Int]] = [:]
+            var newScoresPerDifficulty: [Int: [String: Int]] = [:]
+            for difficulty in 1...12 {
+                newScoresPerDifficulty[difficulty] = ["F": 0, "E": 0, "D": 0, "C": 0, "B": 0, "A": 0, "AA": 0, "AAA": 0]
+                newClearLampPerDifficulty[difficulty] = [
+                    "FULLCOMBO CLEAR": 0,
+                    "CLEAR": 0,
+                    "ASSIST CLEAR": 0,
+                    "EASY CLEAR": 0,
+                    "HARD CLEAR": 0,
+                    "EX HARD CLEAR": 0,
+                    "FAILED": 0
+                ]
+            }
+
+            var scores: [ScoreForLevel] = []
+            for songRecord in songRecords {
+                let scoresAvailable: [ScoreForLevel] = [
+                    songRecord.beginnerScore,
+                    songRecord.normalScore,
+                    songRecord.hyperScore,
+                    songRecord.anotherScore,
+                    songRecord.leggendariaScore
+                ]
+                    .filter({$0.difficulty != 0})
+                scores.append(contentsOf: scoresAvailable)
+            }
+
+            for score in scores {
+                if score.djLevel != "---" {
+                    newScoresPerDifficulty[score.difficulty]?[score.djLevel]? += 1
+                }
+                if score.clearType != "NO PLAY" {
+                    newClearLampPerDifficulty[score.difficulty]?[score.clearType]? += 1
+                }
+            }
+
+            await MainActor.run { [newClearLampPerDifficulty, newScoresPerDifficulty] in
+                withAnimation {
+                    self.clearLampPerDifficulty = newClearLampPerDifficulty
+                    self.scoresPerDifficulty = newScoresPerDifficulty
+                }
+            }
         }
     }
 }
