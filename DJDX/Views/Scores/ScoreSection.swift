21339abfd7365ac9e3875e7cb89f9bde4cad04c0
katagaki <68231035+katagaki@users.noreply.github.com>
2024-06-05 21:16:32 +0900

    Hide history button when no play data is available

--- DJDX/Views/Scores/ScoreSection.swift
+++ DJDX/Views/Scores/ScoreSection.swift
@@ -79,9 +79,11 @@ struct ScoreSection: View {
             HStack(spacing: 16.0) {
                 IIDXLevelLabel(orientation: .horizontal, levelType: score.level, score: score)
                 Spacer()
-                NavigationLink(value: ViewPath.scoreHistory(songTitle: songTitle, level: score.level)) {
-                    Image(systemName: "clock.arrow.circlepath")
-                }.accessibilityLabel("Scores.Viewer.ShowHistory")
+                if score.djLevelEnum() != .none {
+                    NavigationLink(value: ViewPath.scoreHistory(songTitle: songTitle, level: score.level)) {
+                        Image(systemName: "clock.arrow.circlepath")
+                    }.accessibilityLabel("Scores.Viewer.ShowHistory")
+                }
                 Menu {
                     Button("Scores.Viewer.OpenYouTube", image: .listIconYouTube) {
                         let searchQuery: String = "IIDX SP\(score.level.code()) \(songTitle)"
