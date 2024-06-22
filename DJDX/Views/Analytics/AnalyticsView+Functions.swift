521acb547742dc574680a69c7fe3f98bd5d215cb
katagaki <68231035+katagaki@users.noreply.github.com>
2024-06-22 09:31:51 +0900

    Check whether any song records exist before calculating for Analytics tab

--- DJDX/Views/Analytics/AnalyticsView+Functions.swift
+++ DJDX/Views/Analytics/AnalyticsView+Functions.swift
@@ -14,43 +14,49 @@ extension AnalyticsView {
         debugPrint("Calculating new values")
         let songRecords = calendar.latestAvailableIIDXSongRecords(in: modelContext)
             .filter { $0.playType == playTypeToShow }
-        var newClearLampPerDifficulty: [Int: [String: Int]] = [:]
-        var newScoresPerDifficulty: [Int: [IIDXDJLevel: Int]] = [:]
-        for difficulty in difficulties {
-            newScoresPerDifficulty[difficulty] = IIDXDJLevel.sorted
-                .reduce(into: [IIDXDJLevel: Int]()) { $0[$1] = 0 }
-            newClearLampPerDifficulty[difficulty] = IIDXClearType.sortedStringsWithoutNoPlay
-                .reduce(into: [String: Int]()) { $0[$1] = 0 }
-        }
-
-        var scores: [IIDXLevelScore] = []
-        for songRecord in songRecords {
-            let scoresAvailable: [IIDXLevelScore] = [
-                songRecord.beginnerScore,
-                songRecord.normalScore,
-                songRecord.hyperScore,
-                songRecord.anotherScore,
-                songRecord.leggendariaScore
-            ]
-                .filter({$0.difficulty != 0})
-            scores.append(contentsOf: scoresAvailable)
-        }
-
-        for score in scores {
-            if score.djLevelEnum() != .none {
-                newScoresPerDifficulty[score.difficulty]?[score.djLevelEnum()]? += 1
+        if songRecords.count == 0 {
+            withAnimation(.snappy.speed(2.0)) {
+                dataState = .presenting
             }
-            if score.clearType != "NO PLAY" {
-                newClearLampPerDifficulty[score.difficulty]?[score.clearType]? += 1
+        } else {
+            var newClearLampPerDifficulty: [Int: [String: Int]] = [:]
+            var newScoresPerDifficulty: [Int: [IIDXDJLevel: Int]] = [:]
+            for difficulty in difficulties {
+                newScoresPerDifficulty[difficulty] = IIDXDJLevel.sorted
+                    .reduce(into: [IIDXDJLevel: Int]()) { $0[$1] = 0 }
+                newClearLampPerDifficulty[difficulty] = IIDXClearType.sortedStringsWithoutNoPlay
+                    .reduce(into: [String: Int]()) { $0[$1] = 0 }
             }
-        }
 
-        withAnimation(.snappy.speed(2.0)) {
-            clearLampPerDifficulty.removeAll()
-            scoreRatePerDifficulty.removeAll()
-            newClearLampPerDifficulty.forEach { clearLampPerDifficulty[$0] = $1 }
-            newScoresPerDifficulty.forEach { scoreRatePerDifficulty[$0] = $1 }
-            dataState = .presenting
+            var scores: [IIDXLevelScore] = []
+            for songRecord in songRecords {
+                let scoresAvailable: [IIDXLevelScore] = [
+                    songRecord.beginnerScore,
+                    songRecord.normalScore,
+                    songRecord.hyperScore,
+                    songRecord.anotherScore,
+                    songRecord.leggendariaScore
+                ]
+                    .filter({$0.difficulty != 0})
+                scores.append(contentsOf: scoresAvailable)
+            }
+
+            for score in scores {
+                if score.djLevelEnum() != .none {
+                    newScoresPerDifficulty[score.difficulty]?[score.djLevelEnum()]? += 1
+                }
+                if score.clearType != "NO PLAY" {
+                    newClearLampPerDifficulty[score.difficulty]?[score.clearType]? += 1
+                }
+            }
+
+            withAnimation(.snappy.speed(2.0)) {
+                clearLampPerDifficulty.removeAll()
+                scoreRatePerDifficulty.removeAll()
+                newClearLampPerDifficulty.forEach { clearLampPerDifficulty[$0] = $1 }
+                newScoresPerDifficulty.forEach { scoreRatePerDifficulty[$0] = $1 }
+                dataState = .presenting
+            }
         }
     }
 }
