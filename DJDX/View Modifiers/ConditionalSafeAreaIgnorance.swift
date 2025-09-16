a45da5d2bc6ed403cc270795622ec6de346d86e2
Justin <68231035+katagaki@users.noreply.github.com>
2025-09-17 07:35:05 +0900

    Revert 9344abc

--- DJDX/View Modifiers/ConditionalSafeAreaIgnorance.swift
+++ DJDX/View Modifiers/ConditionalSafeAreaIgnorance.swift
@@ -11,7 +11,7 @@ struct ConditionalSafeAreaIgnorance: ViewModifier {
     func body(content: Content) -> some View {
         if #available(iOS 26.0, *) {
             content
-                .ignoresSafeArea(edges: .bottom)
+                .ignoresSafeArea()
         } else {
             content
         }
